3ea3fadbc837f2febd053a048afb4aee2404e35c
Adheenka <adheen991@gmail.com>
2023-07-24 00:02:53 +0530

    21. How To Add Date And Datetime Fields In Odoo

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -4,5 +4,7 @@ from odoo import fields, models
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread','mail.activity.mixin']
-    _description = "hospital appointment"
+    _description = "Hospital Appointment"
     pateint_id = fields.Many2one('hospital.pateint')
+    appointment_time = fields.Datetime(string='Appointment Time')
+    booking_time = fields.Date(string='Booking Date')
