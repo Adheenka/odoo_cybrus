d8190830e1b68383f55d588c4ba51becf476af7e
Adheenka <adheen991@gmail.com>
2023-07-24 17:09:39 +0530

    stash

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -5,6 +5,8 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
-    pateint_id = fields.Many2one('hospital.pateint')
+
+    pateint_id = fields.Many2one('hospital.pateint',string="patient")
+    gender = fields.Selection(related=pateint_id.gender)
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_time = fields.Date(string='Booking Date', default=fields.Date.context_today)
