c29c035cee5a9fbe8286348edee8d34b59e02472
Adheenka <adheen991@gmail.com>
2023-07-24 15:25:58 +0530

    22. How To Set Default Values For Fields In Odoo

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -3,8 +3,8 @@ from odoo import fields, models
 
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
     pateint_id = fields.Many2one('hospital.pateint')
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_time = fields.Date(string='Booking Date')
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_time = fields.Date(string='Booking Date', default=fields.Date.context_today)
