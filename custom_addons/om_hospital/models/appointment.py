dce726debda0cf9a9223d768a63acee4e30484b1
Adheenka <adheen991@gmail.com>
2023-07-25 19:56:50 +0530

    25. How To Define Onchange Functions In Odoo

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api,fields, models
 
 
 class HospitalAppointment(models.Model):
@@ -6,6 +6,11 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
     pateint_id = fields.Many2one('hospital.pateint')
+    ref = fields.Char(string='Reference', tracking=True)
     gender = fields.Selection(related='pateint_id.gender',readonly=False)
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_time = fields.Date(string='Booking Date', default=fields.Date.context_today)
+
+    @api.onchange('pateint_id')
+    def onchange_pateint_id(self):
+        self.ref = self.pateint_id.ref
