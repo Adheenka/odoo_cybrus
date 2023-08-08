6e43daec683106aec65e716b7594bf1561557fcc
Adheenka <adheen991@gmail.com>
2023-08-08 21:06:42 +0530

    57. How To Add Many2many Field In Odoo15

--- custom_addons/om_hospital/models/pateint.py
+++ custom_addons/om_hospital/models/pateint.py
@@ -14,6 +14,7 @@ class HospitalPateint(models.Model):
     gender = fields.Selection([('male','Male'), ('female','Female')],string='Gender',tracking=True)
     active = fields.Boolean(string="Active",default=True)
     image = fields.Image(string="Image")
+    tag_ids = fields.Many2many('patient.tag',string="Tag")
 
     @api.depends('date_of_birth')
     def _compute_age(self):
