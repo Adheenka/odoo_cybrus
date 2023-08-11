6c3d1cdbbb19cabf226aa85a2ad3b014541149c6
Adheenka <adheen991@gmail.com>
2023-08-11 16:26:16 +0530

    65. How To Load Data From XML File In Odoo

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -13,6 +13,7 @@
     'depends': ['mail','product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
         'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/pateint_view.xml',
