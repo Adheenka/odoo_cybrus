ca19d108f1bd81919e623f3acfe888ad4ca5b1db
Adheenka <adheen991@gmail.com>
2023-08-19 22:18:36 +0530

    84. Copy Function In Odoo | Copy Attribute In Odoo

--- custom_addons/om_hospital/models/patient_tag.py
+++ custom_addons/om_hospital/models/patient_tag.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
 
 class PateintTag(models.Model):
@@ -6,11 +6,26 @@ class PateintTag(models.Model):
     _description = "Patient Tag"
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string='Active',default=True)
+    active = fields.Boolean(string='Active',default=True,copy=False)
     color = fields.Integer(string="color")
     color_2 = fields.Char(string="color_2")
     sequence =fields.Integer(string="Sequence")
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if not default.get('name'):
+            default['name'] = _("%s (copy)",self.name)
+        self.sequence = 10
+        return super(PateintTag, self).copy(default)
 
+    # @api.returns('self')
+    # def copy(self, default=None):
+    #     default = default or {}
+    #     if not default.get('name'):
+    #         default['name'] = f"{self.name} (copy)"
+    #     self.sequence = 10
+    #     return super().copy(default)
 
     _sql_constraints = [
         ('unique_name','unique (name)','Name must unique.'),
