4abb6f707bca31ea343aaee42a1adcaf488f02a2
Adheenka <adheen991@gmail.com>
2023-08-19 17:03:48 +0530

    82. Sql Constraints In Odoo || Constrains In Odoo

--- custom_addons/om_hospital/models/patient_tag.py
+++ custom_addons/om_hospital/models/patient_tag.py
@@ -9,3 +9,10 @@ class PateintTag(models.Model):
     active = fields.Boolean(string='Active',default=True)
     color = fields.Integer(string="color")
     color_2 = fields.Char(string="color_2")
+    sequence =fields.Integer(string="Sequence")
+
+
+    _sql_constraints = [
+        ('unique_name','unique (name)','Name must unique.'),
+        ('check_sequence','check(sequence > 0)','Sequence must be non zero..')
+    ]
