f5db0011dc301610aff7cd262e05e8743daee7cb
Adheenka <adheen9961@gmail.com>
2023-08-22 21:07:43 +0530

    task5 1and 2 complete

---
+++ custom_addons/om_odoo_inheritence_task5/models/sale_order.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+
+    custom_user_id = fields.Many2one('res.users', string='Custom User')
+
+
+    def action_confirm(self):
+        res = super(SaleOrderInherit, self).action_confirm()
+        for order in self:
+            order.custom_user_id = self.env.user
+        return res
