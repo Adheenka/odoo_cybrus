452d8f116030f6a43922b73c5c8da09ed2824768
Adheenka <adheen9961@gmail.com>
2023-08-22 22:35:45 +0530

    task12finish

--- custom_addons/om_odoo_inheritence_task5/models/sale_order.py
+++ custom_addons/om_odoo_inheritence_task5/models/sale_order.py
@@ -1,14 +1,12 @@
 from odoo import models, fields, api
 
-
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
     custom_user_id = fields.Many2one('res.users', string='Custom User')
 
-
     def action_confirm(self):
         res = super(SaleOrderInherit, self).action_confirm()
         for order in self:
-            order.custom_user_id = self.env.user
+            order.write({'custom_user_id': self.env.user.id})
         return res
