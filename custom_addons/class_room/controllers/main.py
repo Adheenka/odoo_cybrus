3adac56d36b8229fddfa76b134a90e2a8da2aba3
Adheenka <adheen9961@gmail.com>
2023-08-31 01:25:24 +0530

    task6 website section finshed

--- custom_addons/class_room/controllers/main.py
+++ custom_addons/class_room/controllers/main.py
@@ -10,7 +10,21 @@ class Classroom(http.Controller):
         # patients = request.env['classroom'].sudo().search([])
         return http.request.render('class_room.create_student', {})
 
-    @http.route('/create/webstudent', type="http",auth="public",website=True)
-    def update_patient(self, **kw):
-        request.env['classroom'].sudo().create(kw)
-        return request.render("class_room.student_thanks", {})
+    # @http.route('/create/webstudent', type="http", auth="public",website=True)
+    # def update_patient(self, **kw):
+    #     request.env['classroom'].sudo().create(kw)
+    #     return request.render("class_room.student_thanks", {})
+    @http.route('/create/webstudent', type="http", auth="public", website=True)
+    def create_webstudent(self, **post):
+        student_name = post.get('student_name')
+        email = post.get('email')
+
+        # Create a student record
+        classroom_model = request.env['classroom']
+        new_student = classroom_model.sudo().create({
+            'name': student_name,
+            'email': email,
+            # Add other fields as needed
+        })
+
+        return http.request.render('class_room.student_thanks', {})
