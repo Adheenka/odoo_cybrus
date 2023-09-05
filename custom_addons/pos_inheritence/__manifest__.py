4913b56a75b8a7cfc6b833a58c27263111e75e85
Adheenka <adheen9961@gmail.com>
2023-09-05 10:14:13 +0530

    pos inherit fireststep

---
+++ custom_addons/pos_inheritence/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'pos_inheritence',
+    'version': '1.0.0',
+
+    'author': 'adheen',
+    'sequence': -90,
+    'summary': 'pos_inheritence',
+    'description': """ pos management system
+""",
+    'depends': ['base'],
+    'data': [
+
+        'views/res_partner_view.xml',
+
+
+    ],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+
+
+}
+
---
+++ custom_addons/pos_inheritence/models/res_partner.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+class CustomResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    dob = fields.Date(string='Date of Birth')
+    mobile = fields.Char(string='Mobile Number')
