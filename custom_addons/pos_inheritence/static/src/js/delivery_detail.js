bcfad34c6493f445ad05b0f2b4d253249205065e
Adheenka <adheen9961@gmail.com>
2023-09-10 13:22:01 +0530

    trash sunday1

--- custom_addons/pos_inheritence/static/src/js/delivery_detail.js
+++ custom_addons/pos_inheritence/static/src/js/delivery_detail.js
@@ -1,38 +1,167 @@
-//odoo.define('pos_delivery_details.ActionpadWidget', function (require) {
-//    'use strict';
+//odoo.define('pos_inheritence.DeliveryDetailsButton', function (require) {
+//    "use strict";
 //
 //    const PosComponent = require('point_of_sale.PosComponent');
 //    const Registries = require('point_of_sale.Registries');
-//    const DeliveryDetailsPopup = require('pos_delivery_details.DeliveryDetailsPopup');
+//    const { useListener } = require('@web/core/utils/hooks');
+//    const { _t } = require('web.core');
 //
-//    class ActionpadWidget extends PosComponent {
+//   class DeliveryDetailsButton extends PosComponent {
 //        constructor() {
 //            super(...arguments);
+//            useListener('click', this.openDeliveryDetailsPopup);
 //        }
 //
 //        async openDeliveryDetailsPopup() {
-//            const { confirmed, payload } = await this.showPopup(DeliveryDetailsPopup, {});
-//            if (confirmed) {
-//                // Handle the payload (delivery details) here
-//                const { deliveryCountry, deliveryType, expectedDeliveryDate } = payload;
-//                // You can now save these details to your custom model or perform any other action.
-//            }
+//            // Add your code to open the delivery details popup here
+//            // For example, you can show a custom popup or perform any other action
+//            console.log('Delivery Details button clicked');
 //        }
 //    }
 //
-//    ActionpadWidget.template = 'ActionpadWidget';
-//    ActionpadWidget.defaultProps = {
-//        isActionButtonHighlighted: false,
-//    }
+//    DeliveryDetailsButton.template = 'DeliveryDetailsButton';
 //
-//    Registries.Component.add(ActionpadWidget);
+//    Registries.Component.add(DeliveryDetailsButton);
 //
-//    return ActionpadWidget;
+//    return DeliveryDetailsButton;
 //});
-//async onClick() {
-//    const selectedOrder = this.env.pos.get_order();
-//    if (selectedOrder) {
-//        // Show the delivery details popup when the button is clicked
-//        this.showPopup('view_delivery_details_popup');
+
+//odoo.define("pos_inheritence.WBSampleButton", function(require){
+//"use strict";
+//
+//    const PosComponent = require("point_of_sale.PosComponent");
+//    const ProductScreen = require("point_of_sale.ProductScreen");
+//    const Registries = require("point_of_sale.Registries");
+//    const { useListener } = require("@web/core/utils/hooks");
+//    const core  = require("web.core");
+//    var _t = core._t;
+//
+//    class WBSampleButton extends PosComponent {
+//
+//        setup(){
+//            super.setup();
+//            useListener("click", this.wb_sample_button_click);
+//        }
+//
+//        async wb_sample_button_click(){
+//
+//            var multi_lang = await this.rpc({
+//                route:"/pos/rpc/example",
+//                params:{},
+//            })
+//
+//
+//            console.log("language ---> ", multi_lang);
+//
+//            var multi_lang_list = [];
+//
+//            multi_lang.forEach(function(value){
+//                   multi_lang_list.push({"id": value.id,
+//                   "label":value.name,
+//                   "item": value});
+//            });
+//
+//            console.log(multi_lang_list);
+//
+//            var {confirmed, payload: selectedOption} = await this.showPopup("SelectionPopup", {
+//                title: "Active Languages!",
+//                list: multi_lang_list
+//            })
+//
+//            console.log(confirmed, selectedOption);
+//
+//
+//            console.log("Hello this is button click event pressed........");
+//        }
+//
 //    }
-//}
+//
+//    WBSampleButton.template = "WBSampleButton";
+//    ProductScreen.addControlButton({
+//        component: WBSampleButton,
+//        position: ["before", "OrderlineCustomerNoteButton"],
+//    });
+//
+//    Registries.Component.add(WBSampleButton);
+//
+//    return WBSampleButton;
+//
+//});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////odoo.define('pos_delivery_details.ActionpadWidget', function (require) {
+////    'use strict';
+////
+////    const PosComponent = require('point_of_sale.PosComponent');
+////    const Registries = require('point_of_sale.Registries');
+////    const DeliveryDetailsPopup = require('pos_delivery_details.DeliveryDetailsPopup');
+////
+////    class ActionpadWidget extends PosComponent {
+////        constructor() {
+////            super(...arguments);
+////        }
+////
+////        async openDeliveryDetailsPopup() {
+////            const { confirmed, payload } = await this.showPopup(DeliveryDetailsPopup, {});
+////            if (confirmed) {
+////                // Handle the payload (delivery details) here
+////                const { deliveryCountry, deliveryType, expectedDeliveryDate } = payload;
+////                // You can now save these details to your custom model or perform any other action.
+////            }
+////        }
+////    }
+////
+////    ActionpadWidget.template = 'ActionpadWidget';
+////    ActionpadWidget.defaultProps = {
+////        isActionButtonHighlighted: false,
+////    }
+////
+////    Registries.Component.add(ActionpadWidget);
+////
+////    return ActionpadWidget;
+////});
+////async onClick() {
+////    const selectedOrder = this.env.pos.get_order();
+////    if (selectedOrder) {
+////        // Show the delivery details popup when the button is clicked
+////        this.showPopup('view_delivery_details_popup');
+////    }
+////}
