4b003e98112588ad96bd48463d37df0b97b352d0
Adheenka <adheen9961@gmail.com>
2023-09-19 11:29:58 +0530

    project estimation

--- custom_addons/pos_inheritence/models/pos_order.py
+++ custom_addons/pos_inheritence/models/pos_order.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 
+
 class PosOrder(models.Model):
     _inherit = 'pos.order'
 
@@ -13,4 +14,28 @@ class PosOrder(models.Model):
     card_number = fields.Char('Card Number')
     expiry_date = fields.Date('Expiry Date')
 
+    @api.model
+    def _default_delivery_country(self):
+        partner = self.env.user.company_id.partner_id
+        return partner.country_id
 
+    def _get_delivery_details_popup(self):
+        popup = self.env.ref('pos_inheritence.DeliveryDetailsPopup')
+        return popup
+
+    def action_show_delivery_details_popup(self):
+        popup = self._get_delivery_details_popup()
+        popup.show()
+
+    def _action_apply_delivery_details(self, values):
+        self.delivery_country = values['delivery_country_id']
+        self.delivery_type = values['delivery_type']
+        self.expected_delivery_date = values['expected_delivery_date']
+
+    def _get_delivery_details(self):
+        delivery_details = {
+            'delivery_country_id': self.delivery_country.id,
+            'delivery_type': self.delivery_type,
+            'expected_delivery_date': self.expected_delivery_date,
+        }
+        return delivery_details
--- custom_addons/pos_inheritence/static/src/js/DeliveryDetailsPopup.js
+++ custom_addons/pos_inheritence/static/src/js/DeliveryDetailsPopup.js
@@ -1,59 +1,18 @@
-odoo.define("point_of_sale.DeliveryDetailsPopup", function (require) {
+odoo.define("pos_inheritence.DeliveryDetailsPopup", function (require) {
     "use strict";
-    const { useState, useRef, onPatched, useComponent} = owl.hooks;
+    const { useState } = owl.hooks;
     const AbstractAwaitablePopup = require("point_of_sale.AbstractAwaitablePopup");
-    const PosComponent = require("point_of_sale.PosComponent");
-    const ProductScreen = require("point_of_sale.ProductScreen");
-    const { useListener } = require("web.custom_hooks");
     const Registries = require("point_of_sale.Registries");
 
-//    class DeliveryDetailsPopup extends AbstractAwaitablePopup {
-//        constructor() {
-//            super(...arguments);
-//            this.state = useState({ inputValue: this.props.startingValue });
-//            this.inputCountry = useRef('inputCountry');
-//            this.inputDeliveryType = useRef('inputDeliveryType');
-//            this.inputDeliveryDate = useRef('inputDeliveryDate');
-//            this.inputDatePicker = null;
-//        }
-//
-//        mounted() {
-//            this.inputCountry.el.focus();
-//            this.initializeDatePicker();
-//        }
-//
-//        async confirm() {
-//            const payload = {
-//                delivery_country: this.state.delivery_country,
-//                delivery_type: this.state.delivery_type,
-//                expected_delivery_date: this.state.expected_delivery_date,
-//            };
-//            this.trigger('delivery-details-confirmed', payload);
-//            this.trigger('close-popup');
-//        }
-//
-//        initializeDatePicker() {
-//            var self = this;
-//            this.inputDatePicker = new Pikaday({
-//                field: this.inputDeliveryDate.el,
-//            });
-//        }
-//    }
-//
-//    DeliveryDetailsPopup.template = 'DeliveryDetailsPopup';
-//
-//    Registries.Component.add(DeliveryDetailsPopup);
-//
-//    return DeliveryDetailsPopup;
-//});
-
-     class DeliveryDetailsPopup extends AbstractAwaitablePopup {
+    class DeliveryDetailsPopup extends AbstractAwaitablePopup {
         constructor() {
             super(...arguments);
-            this.state = {
+            this.state = useState({
                 delivery_country_id: null,
                 delivery_country_name: "",
-            };
+                delivery_type: "domestic",
+                expected_delivery_date: new Date(),
+            });
         }
 
         openCountrySelectionPopup() {
@@ -67,13 +26,26 @@ odoo.define("point_of_sale.DeliveryDetailsPopup", function (require) {
             this.state.delivery_country_name = selectedCountry.name;
         }
 
-        getPayload() {
-            return {
+        async confirm() {
+            const payload = {
                 delivery_country_id: this.state.delivery_country_id,
                 delivery_country_name: this.state.delivery_country_name,
-                delivery_type: this.el.querySelector("#delivery_type").value,
-                expected_delivery_date: this.el.querySelector("#expected_delivery_date").value,
+                delivery_type: this.state.delivery_type,
+                expected_delivery_date: this.state.expected_delivery_date.toISOString(),
             };
+
+            // Create a new record in the DeliveryDetails model
+            const deliveryDetails = await this.rpc({
+                model: 'pos.order',
+                method: 'create',
+                args: [payload],
+            });
+
+            // Link the delivery details to the POS order
+            const order = this.env.pos.get_order();
+            order.delivery_details_id = deliveryDetails;
+
+            this.trigger('close-popup');
         }
     }
 
@@ -83,6 +55,169 @@ odoo.define("point_of_sale.DeliveryDetailsPopup", function (require) {
 
     return DeliveryDetailsPopup;
 });
+
+
+
+
+
+
+
+//odoo.define("pos_inheritence.DeliveryDetailsPopup", function (require) {
+//    "use strict";
+//    const { useState } = owl.hooks;
+//    const AbstractAwaitablePopup = require("point_of_sale.AbstractAwaitablePopup");
+//    const PosComponent = require("point_of_sale.PosComponent");
+//    const ProductScreen = require("point_of_sale.ProductScreen");
+//    const { useListener } = require("web.custom_hooks");
+//    const Registries = require("point_of_sale.Registries");
+//
+//
+//    class DeliveryDetailsPopup extends AbstractAwaitablePopup {
+//        constructor() {
+//            super(...arguments);
+//            this.state = useState({
+//                delivery_country_id: null,
+//                delivery_country_name: "",
+//                delivery_type: "domestic",
+//                expected_delivery_date: new Date(),
+//            });
+//        }
+//
+//        openCountrySelectionPopup() {
+//            this.showPopup("CountrySelectionPopup", {
+//                confirm: this.confirmCountrySelection.bind(this),
+//            });
+//        }
+//
+//        confirmCountrySelection(selectedCountry) {
+//            this.state.delivery_country_id = selectedCountry.id;
+//            this.state.delivery_country_name = selectedCountry.name;
+//        }
+//
+//        getPayload() {
+//            return {
+//                delivery_country_id: this.state.delivery_country_id,
+//                delivery_country_name: this.state.delivery_country_name,
+//                delivery_type: this.state.delivery_type,
+//                expected_delivery_date: this.state.expected_delivery_date.toISOString(),
+//            };
+//        }
+//    }
+//
+//    DeliveryDetailsPopup.template = "DeliveryDetailsPopup";
+//
+//    Registries.Component.add(DeliveryDetailsPopup);
+//
+//    return DeliveryDetailsPopup;
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//errorcode
+
+
+//odoo.define('point_of_sale.DeliveryDetailsPopup', function (require) {
+//    'use strict';
+//
+//    const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
+//    const Registries = require('point_of_sale.Registries');
+//    const { useState, useRef } = owl;
+//    const { _lt } = require('@web/core/l10n/translation');
+//    const { Gui } = require('point_of_sale.Gui');
+//
+//    class DeliveryDetailsPopup extends AbstractAwaitablePopup {
+//        constructor() {
+//            super(...arguments);
+//            this.state = useState({
+//                deliveryCountryId: null,
+//                deliveryCountryName: '',
+//                deliveryType: '',
+//                expectedDeliveryDate: '',
+//            });
+//            this.deliveryTypeRef = useRef('deliveryType');
+//            this.expectedDeliveryDateRef = useRef('expectedDeliveryDate');
+//        }
+//
+//        openCountrySelectionPopup() {
+//            this.showPopup('CountrySelectionPopup', {
+//                confirm: this.confirmCountrySelection.bind(this),
+//            });
+//        }
+//
+//        confirmCountrySelection(selectedCountry) {
+//            this.state.deliveryCountryId = selectedCountry.id;
+//            this.state.deliveryCountryName = selectedCountry.name;
+//        }
+//
+//        async confirm() {
+//            if (!this.state.deliveryCountryId || !this.state.deliveryType || !this.state.expectedDeliveryDate) {
+//                this.showError('Incomplete Details', 'Please fill in all required fields.');
+//                return;
+//            }
+//
+//            const payload = {
+//                delivery_country_id: this.state.deliveryCountryId,
+//                delivery_country_name: this.state.deliveryCountryName,
+//                delivery_type: this.state.deliveryType,
+//                expected_delivery_date: this.state.expectedDeliveryDate,
+//            };
+//
+//            this.trigger('delivery-details-confirmed', payload);
+//            this.trigger('close-popup');
+//        }
+//
+//        showError(title, body) {
+//            Gui.showPopup('ErrorPopup', {
+//                title: _lt(title),
+//                body: _lt(body),
+//            });
+//        }
+//
+//        getPayload() {
+//            return {
+//                delivery_country_id: this.state.deliveryCountryId,
+//                delivery_country_name: this.state.deliveryCountryName,
+//                delivery_type: this.state.deliveryType,
+//                expected_delivery_date: this.state.expectedDeliveryDate,
+//            };
+//        }
+//    }
+//
+//    DeliveryDetailsPopup.template = 'DeliveryDetailsPopup';
+//
+//    Registries.Component.add(DeliveryDetailsPopup);
+//
+//    return DeliveryDetailsPopup;
+//});
+//
+
+
+
+
+
+
+
+
+
 
 
 
