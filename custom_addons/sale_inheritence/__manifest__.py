e6fff58af7ec444525db44127cb208f1e7488f57
Adheenka <adheen9961@gmail.com>
2023-09-20 11:02:46 +0530

    menu is rdy:

---
+++ custom_addons/sale_inheritence/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'sale_inheritence',
+    'version': '1.0.0',
+    'category': 'Sales/Sales',
+    'author': 'adheen',
+    'sequence': -90,
+    'summary': 'sa8e_inheritence',
+    'description': """ sale management system
+""",
+    'depends': ['base','sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_view.xml',
+
+
+
+
+
+
+
+
+    ],
+    # 'assets':{
+    #     'web.assets_qweb': ['pos_inheritence/static/src/xml/*',
+    #                         ],
+    #     'web.assets_backend': ['pos_inheritence/static/src/js/*',
+    #                            ],
+    # },
+
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+
+
+}
+
