bc2477a99c5061e309acbf0528134f95422d59af
Adheenka <adheen9961@gmail.com>
2023-09-26 16:22:08 +0530

    sequence is redy

--- custom_addons/sale_inheritence/models/sale_order_inherit.py
+++ custom_addons/sale_inheritence/models/sale_order_inherit.py
@@ -15,28 +15,27 @@ class JobOrder(models.Model):
     job_no = fields.Char(string='Job No')
     customer_name = fields.Many2one('res.partner', string='Customer Name')
     job_order_lines = fields.One2many('sale.order.line', 'job_order_id', string='Job Order Lines')
-    job_order_id = fields.Many2one('sale.order.line', string='estimation')
+    # job_order_id = fields.Many2one('sale.order.line', string='estimation')
 
-    # name = fields.Char(string='Job Order No.', required=True, copy=False, readonly=True,
-    #                    default=lambda self: _('New'), index=True)
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     #
-    # job_order_id = fields.Many2one('job.order', string='Job Order')
+    job_order_id = fields.Many2one('job.order', string='Job Order')
     quantity = fields.Float(string="Quantity")
     seq = fields.Integer(string='Serial No', compute='_compute_serial_number', readonly=True)
 
 
-    @api.depends('product_id')
+
     def _compute_serial_number(self):
         for line in self:
-            no = 1  # Initialize the serial number to 1
-            for l in line.product_id:
-                l.seq = no
+            no = 0
+            line.seq = no
+            for i in line.order_id.order_line:
                 no += 1
+                i.seq = no
     # def action_create_job_order(self):
     #
     #     job_order_obj = self.env['job.order']
