ff16438696b2ec8fe31347536fa25e423d952b6b
Adheenka <adheen9961@gmail.com>
2023-10-25 01:37:01 +0530

    tasah

--- custom_addons/sale_inheritence/models/account_move_inherit.py
+++ custom_addons/sale_inheritence/models/account_move_inherit.py
@@ -53,3 +53,9 @@ class AccountMoveForm(models.Model):
                 self.invoice_line_ids += invoice_line
     #for expence code
     is_expense = fields.Boolean(string="Is Expense", default=False)
+    expense_sequence = fields.Char(string="Expense Sequence")
+    @api.model
+    def create(self, vals):
+        if vals.get("move_type") == "in_invoice" and vals.get("is_expense"):
+            vals["name"] = self.env["ir.sequence"].next_by_code("account.move.expense.sequence") or "New"
+        return super(AccountMoveForm, self).create(vals)
