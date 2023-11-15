dc8bada06c9f4f18266845e721536042c33ba312
Adheenka <adheen9961@gmail.com>
2023-11-15 22:00:35 +0530

    hr module skelton is redy

---
+++ custom_addons/embloyees_attendence/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'embloyees_sheduler_module',
+    'version': '1.0.0',
+    'category': 'Human Resources/Attendances',
+    'author': 'adheen',
+    'sequence': -93,
+    'summary': 'employee_attendence_automation',
+    'description': """ for emloyees module sheduler action system""",
+    'depends': ['hr'],
+    'data': [
+
+        'data/seduler.xml',
+
+
+
+
+    ],
+
+
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+
+
+}
+
---
+++ custom_addons/embloyees_attendence/models/hr_account_inherit.py
@@ -0,0 +1,8 @@
+from odoo import fields, models, api ,_
+from odoo.exceptions import ValidationError
+from odoo.tools import format_date
+
+
+# from odoo.addons.sale.models.sale_order import SaleOrder
+
+
