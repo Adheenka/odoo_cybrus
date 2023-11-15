f3389168fe919b2e204bd2558d7a007d3dd2713e
Adheenka <adheen9961@gmail.com>
2023-11-16 01:58:18 +0530

    half is redy

--- custom_addons/embloyees_attendence/__manifest__.py
+++ custom_addons/embloyees_attendence/__manifest__.py
@@ -8,7 +8,7 @@
     'sequence': -93,
     'summary': 'employee_attendence_automation',
     'description': """ for emloyees module sheduler action system""",
-    'depends': ['hr'],
+    'depends': ['hr','hr_attendance'],
     'data': [
 
         'data/seduler.xml',
--- custom_addons/embloyees_attendence/models/hr_account_inherit.py
+++ custom_addons/embloyees_attendence/models/hr_account_inherit.py
@@ -1,8 +1,31 @@
 from odoo import fields, models, api ,_
 from odoo.exceptions import ValidationError
-from odoo.tools import format_date
+from datetime import datetime, timedelta
 
+class HrAttendance(models.Model):
+    _inherit = "hr.attendance"
 
-# from odoo.addons.sale.models.sale_order import SaleOrder
+    def automatic_check_out(self):
+        # Find the latest attendance record for the current employee
+        latest_attendance = self.search([
+            ('employee_id', '=', self.env.user.employee_id.id),
+        ], order='check_in desc', limit=1)
 
+        if latest_attendance:
+            # Retrieve the working hours per day from the employee's calendar
+            working_hours_per_day = latest_attendance.employee_id.resource_calendar_id.hours_per_day
 
+            # Calculate the threshold for automatic checkout
+            automatic_checkout_threshold = timedelta(hours=working_hours_per_day)
+
+            # Calculate the time elapsed since check-in
+            time_elapsed = datetime.now() - latest_attendance.check_in
+
+            if time_elapsed >= automatic_checkout_threshold:
+                # Auto checkout if the time elapsed exceeds the threshold
+                latest_attendance.check_out = datetime.now()
+
+        # Perform other necessary operations if needed
+        # ...
+
+        return True
