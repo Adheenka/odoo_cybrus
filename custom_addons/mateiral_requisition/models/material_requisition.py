9d96cce98cc7128177ef55a67ae54b995b18e6c5
Adheenka <adheen9961@gmail.com>
2023-11-22 21:13:00 +0530

    redy

--- custom_addons/mateiral_requisition/models/material_requisition.py
+++ custom_addons/mateiral_requisition/models/material_requisition.py
@@ -22,7 +22,43 @@ class MaterialRequisition(models.Model):
     materials_line_ids = fields.One2many('materials','material_requisition_id',string='Requisition Lines')
     location = fields.Many2one('stock.location', string='Stock Location')
 
+    material_requisition_ids = fields.One2many('stock.picking','material_requisition_id', string='Stock Pickings',readonly=True)
+    picking_count = fields.Integer('Picking Count', compute='_compute_picking_count')
 
+    def _compute_picking_count(self):
+        for requisition in self:
+            requisition.picking_count = len(requisition.material_requisition_ids )
+    # def _compute_picking_count(self):
+    #     picking_data = self.env['stock.picking'].with_context(active_test=False).read_group([], [], [])
+    #     total_count = sum(data.get('__count', 0) for data in picking_data)
+    #     for record in self:
+    #         record.picking_count = total_count
+
+
+    # def get_picking(self):
+    #     pickings = self.env['stock.picking'].search([('material_requisition_id', '!=', False)])
+    #     action = {
+    #         'type': 'ir.actions.act_window',
+    #         'name': 'Internal Pickings',
+    #         'view_mode': 'tree,form',
+    #         'res_model': 'stock.picking',
+    #         'view_id': False,
+    #         'domain': [('material_requisition_id', '!=', False)],
+    #     }
+    #     return action
+    def get_picking(self):
+        pickings = self.env['stock.picking'].search([('material_requisition_id', '=', self.id)])
+        action = {
+            'type': 'ir.actions.act_window',
+            'name': 'Internal Pickings',
+            'view_mode': 'form',
+            'res_model': 'stock.picking',
+            'view_id': False,
+            'domain': [('material_requisition_id', '=', self.id)],
+        }
+        if pickings:
+            action.update({'res_id': pickings[0].id})  # Open the first record if found
+        return action
     @api.model
     def create(self, vals):
         if vals.get('sequence', 'New') == 'New':
@@ -36,22 +72,8 @@ class MaterialRequisition(models.Model):
 
         return super(MaterialRequisition, self).create(vals)
 
-    def show_picking(self):
 
-        return {
-            'type': 'ir.actions.act_window',
-            'name': _('Internal Picking'),
-            'res_model': 'stock.picking',
-            'view_mode': 'list,form',
-            'domain': [('material_requisition_id', 'in', self.ids)],  # Adjust the domain as needed
-        }
 
-    # def show_picking(self):
-    #     for rec in self:
-    #         res = self.env.ref('stock.action_picking_tree_all')
-    #         res = res.read()[0]
-    #         res['domain'] = str([('custom_requisition_id', '=', rec.id)])
-    #     return res
 
 
     def send_request(self):
@@ -65,6 +87,55 @@ class MaterialRequisition(models.Model):
     def send_email_notification(self):
         mail_template = self.env.ref('mateiral_requisition.email_template_material_requisition')
         mail_template.send_mail(self.id, force_send=True)
+
+
+    def requisition_confirm(self):
+
+        pass
+
+    def approve_request(self):
+        # Update the stage to 'Inventory Confirmed'
+        for rec in self:
+            rec.write({'stage': 'inventory_confirmed'})
+
+        # Create a picking record
+        stock_picking_vals = {
+            'partner_id': rec.employee_id.id,
+            'origin': rec.sequence,
+            'scheduled_date': rec.requisition_date,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'location_dest_id': self.env.ref('stock.stock_location_customers').id,
+            'picking_type_id': self.env.ref('stock.picking_type_out').id,
+            'material_requisition_id': rec.id,
+        }
+        stock_picking = self.env['stock.picking'].create(stock_picking_vals)
+
+        # Create move lines for the picking
+        move_vals = []
+        for line in rec.materials_line_ids:
+            move_vals.append((0, 0, {
+                'product_id': line.product_id.id,
+                'name': line.description,
+                'product_uom_qty': line.quantity,
+                'product_uom': 1,
+                'location_id': self.env.ref('stock.stock_location_stock').id,
+                'location_dest_id': self.env.ref('stock.stock_location_customers').id,
+                'picking_type_id': self.env.ref('stock.picking_type_out').id,
+            }))
+
+        stock_picking.write({'move_ids_without_package': move_vals})
+
+        return {
+            'name': 'Stock Picking',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'stock.picking',
+            'res_id': stock_picking.id,
+            'type': 'ir.actions.act_window',
+            'target': 'self',
+        }
+
+
     # def approve_request(self):
     #     # Update the stage to 'Inventory Confirmed'
     #     for rec in self:
@@ -77,23 +148,20 @@ class MaterialRequisition(models.Model):
     #         'location_id': self.env.ref('stock.stock_location_stock').id,  # Default Stock Location
     #         'location_dest_id': self.env.ref('stock.stock_location_customers').id,  # Default Customers Location
     #         'picking_type_id': self.env.ref('stock.picking_type_out').id,
+    #         'material_requisition_id': rec.id,
     #
-    #
-    #
-    #     }
-    #     stock_moves = []
-    #     for line in rec.materials_line_ids:
-    #         stock_move_vals = {
-    #             'name': line.description,
+    #         'move_ids_without_package': [(0, 0, {
     #             'product_id': line.product_id.id,
+    #             'name': line.description,
     #             'product_uom_qty': line.quantity,
     #             'product_uom': 1,
     #             'location_id': self.env.ref('stock.stock_location_stock').id,  # Default Stock Location
     #             'location_dest_id': self.env.ref('stock.stock_location_customers').id,  # Default Customers Location
     #             'picking_type_id': self.env.ref('stock.picking_type_out').id,
-    #             'sale_line_id': False,  # You may set it based on your needs
-    #         }
-    #         stock_moves.append(self.env['stock.move'].create(stock_move_vals))
+    #
+    #         }) for line in self.materials_line_ids],
+    #     }
+    #
     #     stock_picking = self.env['stock.picking'].create(stock_picking_vals)
     #
     #     return {
@@ -105,47 +173,6 @@ class MaterialRequisition(models.Model):
     #         'type': 'ir.actions.act_window',
     #         'target': 'self',
     #     }
-
-    def requisition_confirm(self):
-
-        pass
-
-    def approve_request(self):
-        # Update the stage to 'Inventory Confirmed'
-        for rec in self:
-            rec.write({'stage': 'inventory_confirmed'})
-
-        stock_picking_vals = {
-            'partner_id': self.employee_id.id,
-            'name': self.sequence,
-            'scheduled_date': self.requisition_date,
-            'location_id': self.env.ref('stock.stock_location_stock').id,  # Default Stock Location
-            'location_dest_id': self.env.ref('stock.stock_location_customers').id,  # Default Customers Location
-            'picking_type_id': self.env.ref('stock.picking_type_out').id,
-
-            'move_ids_without_package': [(0, 0, {
-                'product_id': line.product_id.id,
-                'name': line.description,
-                'product_uom_qty': line.quantity,
-                'product_uom': 1,
-                'location_id': self.env.ref('stock.stock_location_stock').id,  # Default Stock Location
-                'location_dest_id': self.env.ref('stock.stock_location_customers').id,  # Default Customers Location
-                'picking_type_id': self.env.ref('stock.picking_type_out').id,
-
-            }) for line in self.materials_line_ids],
-        }
-
-        stock_picking = self.env['stock.picking'].create(stock_picking_vals)
-
-        return {
-            'name': 'Stock Picking',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'stock.picking',
-            'res_id': stock_picking.id,
-            'type': 'ir.actions.act_window',
-            'target': 'self',
-        }
 
 
 class Materials(models.Model):
@@ -176,4 +203,8 @@ class Materials(models.Model):
             self.unit_price = self.product_id.list_price
             self.description = self.product_id.name
 
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    material_requisition_id = fields.Many2one('material.requisition', string='Material Requisition', ondelete="cascade")
 
